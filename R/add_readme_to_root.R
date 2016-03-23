c83019c003cdeb192f15da6665f10f3716eb8b9f
Jose Eduardo Meireles <jemeireles@gmail.com>
2016-03-23 11:19:16 -0500

    remove root README

--- R/add_readme_to_root.R
+++
@@ -1,6 +0,0 @@
-library("rmarkdown")
-
-rmarkdown::render("vignettes/introduction_to_fia.Rmd",
-                  output_format = "md_document",
-                  output_file = "README.md",
-                  output_dir = getwd() )
